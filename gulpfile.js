32836b1d04aafcc9c1522a0c112a62bb0adb559e
Shuriken (ACER) <tarabet@yandex.ru>
2016-01-25 15:26:41 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,85 @@
+/**
+ * Created by Shuriken on 16.01.2016.
+ */
+
+var gulp = require('gulp'),
+    minifycss = require('gulp-minify-css'),
+    jshint = require('gulp-jshint'),
+    stylish = require('jshint-stylish'),
+    uglify = require('gulp-uglify'),
+    usemin = require('gulp-usemin'),
+    imagemin = require('gulp-imagemin'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    changed = require('gulp-changed'),
+    rev = require('gulp-rev'),
+    browserSync = require('browser-sync'),
+    del = require('del'),
+    ngannotate = require('gulp-ng-annotate');
+
+gulp.task('jshint', function() {
+    return gulp.src('app/scripts/**/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter(stylish));
+});
+
+gulp.task('clean', function() {
+    return del(['dist']);
+});
+
+gulp.task('default', ['clean'], function() {
+   gulp.start('usemin', 'imagemin', 'copyfonts');
+});
+
+gulp.task('usemin', ['jshint'], function() {
+    return gulp.src('./app/menu.html')
+        .pipe(usemin({
+            css: [minifycss(), rev()],
+            js: [ngannotate(), uglify(), rev()]
+        }))
+        .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('imagemin', function() {
+    return del(['dist/images']), gulp.src('app/images/**.*')
+        .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+        .pipe(gulp.dest('dist/images'))
+        .pipe(notify({ message: 'Images task completed!' }));
+});
+
+gulp.task('copyfonts', ['clean'],  function() {
+    gulp.src('./bower_components/font-awesome/fonts/**/*.{ttf,woff,eof,svg}*')
+        .pipe(gulp.dest('./dist/fonts'));
+    gulp.src('./bower_components/bootstrap/dist/fonts/**/*.{ttf,woff,eof,svg}*')
+        .pipe(gulp.dest('./dist/fonts'));
+});
+
+gulp.task('watch', ['browser-sync'], function() {
+    //gulp.watch(['app/scripts/**/*.js', 'app/styles/**/*.css', 'app/**/*.html'], ['usemin']);
+    gulp.watch('{app/scripts/**/*.js,app/styles/**/*.css,app/**/*.html}', ['usemin']);
+    gulp.watch('app/images/**/*', ['imagemin']);
+});
+
+gulp.task('browser-sync', ['default'], function() {
+    var files = [
+        'app/**/*.html',
+        'app/styles/**/*.css',
+        'app/scripts/**/*.js',
+        'app/images/**/*.png',
+        'dist/**/*'
+    ];
+
+    browserSync.init(files, {
+
+        server: {
+            baseDir: 'dist',
+            index: 'menu.html'
+        },
+
+        reloadDelay: 1000
+    });
+
+    gulp.watch(['dist/**/*.*']).on('change', browserSync.reload);
+});
